3c63c0f18e1eda877fb5e78fc462faf029c025fa
Jose Fortes <jose.fortes.neto@gmail.com>
2018-05-12 23:03:45 -0300

    Ajustes index manager

--- src/KWIC/IndexManager.java
+++ src/KWIC/IndexManager.java
@@ -1,19 +1,28 @@
 package KWIC;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class IndexManager {
 
-    Map<> map = new HashMap<>();
+    Map<String, List<Integer>> map = new HashMap<String, List<Integer>>();
     public boolean isEmpty(){
+
         return false;
     }
 
-    public void map(){
-
+    public void initMap(String word, String line, int pos){
+        if (map.containsValue(word)){
+            map.put(word, List.add(pos));
+        }else{
+            map.put(word, List.add(pos));
+        }
     }
 
-    public void ocurrencesOfWord(){}
+    public void ocurrencesOfWord(String word){
+
+    }
 
     public void sorteWords(){}
 }
