5c0e294ae2d8b78351c5ba0d5edbf33f6142e9fb
Jose Fortes <jose.fortes.neto@gmail.com>
2018-05-13 11:24:13 -0300

    Alteracoes

--- src/KWIC/IndexManager.java
+++ src/KWIC/IndexManager.java
@@ -1,28 +1,37 @@
 package KWIC;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class IndexManager {
 
-    Map<String, List<Integer>> map = new HashMap<String, List<Integer>>();
+    Map<String, List<IndexStorage>> map = new HashMap<String, List<IndexStorage>>();
     public boolean isEmpty(){
 
         return false;
     }
 
     public void initMap(String word, String line, int pos){
-        if (map.containsValue(word)){
-            map.put(word, List.add(pos));
-        }else{
-            map.put(word, List.add(pos));
-        }
+
+        IndexStorage index = new IndexStorage();
+        index.setLine(line);
+        index.setPosition(pos);
+
+        map.put(word, List.add(index));
     }
 
-    public void ocurrencesOfWord(String word){
-
+    public List<IndexStorage> ocurrencesOfWord(String word){
+        return map.get(word);
     }
 
-    public void sorteWords(){}
+    public List<String> sorteWords(){
+        List<String> list = new List<String>();
+        map.forEach((d) -> {
+            list.add(d);
+        });
+        Collections.sort(list);
+        return list;
+    }
 }
