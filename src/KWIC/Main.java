cdb67555d4a16994d468e070853e2e245a537eb6
Jose Fortes <jose.fortes.neto@gmail.com>
2018-05-13 18:05:02 -0300

    Implementada feature de busca no banco do site DBLP

--- src/KWIC/Main.java
+++ src/KWIC/Main.java
@@ -10,33 +10,38 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Scanner ler = new Scanner(System.in);
 
-        System.out.printf("nome de arquivo:\n");
-        String nome = ler.nextLine();
 
-        //System.out.printf("\nConteúdo do texto:\n");
-        try
-        {
-            FileReader arq = new FileReader(nome);
-            BufferedReader lerArq = new BufferedReader(arq);
 
-            String linha = lerArq.readLine();
-
-            while (linha != null)
-            {
-                //Manda linha para gerar shifts:
-                WordShift.shift(linha);
-
-                linha = lerArq.readLine();
-            }
-
-            arq.close();
-        } catch (IOException e)
-        {
-            System.err.printf("Erro ao abrir: %s.\n",
-                    e.getMessage());
-        }
+        DataStorageManager data = new DBLPStorageManager();
+        data.init("Carro");
+//        Scanner ler = new Scanner(System.in);
+//
+//        System.out.printf("nome de arquivo:\n");
+//        String nome = ler.nextLine();
+//
+//        //System.out.printf("\nConteúdo do texto:\n");
+//        try
+//        {
+//            FileReader arq = new FileReader(nome);
+//            BufferedReader lerArq = new BufferedReader(arq);
+//
+//            String linha = lerArq.readLine();
+//
+//            while (linha != null)
+//            {
+//                //Manda linha para gerar shifts:
+//                WordShift.shift(linha);
+//
+//                linha = lerArq.readLine();
+//            }
+//
+//            arq.close();
+//        } catch (IOException e)
+//        {
+//            System.err.printf("Erro ao abrir: %s.\n",
+//                    e.getMessage());
+//        }
 
         System.out.println();
     }
