a8e05aba8ff625bdbbac88e26d980af2036a5e9c
Jose Fortes <jose.fortes.neto@gmail.com>
2018-05-13 11:30:58 -0300

    Alteracao temporaria na indexManager

--- src/KWIC/IndexManager.java
+++ src/KWIC/IndexManager.java
@@ -7,31 +7,31 @@ import java.util.Map;
 
 public class IndexManager {
 
-    Map<String, List<IndexStorage>> map = new HashMap<String, List<IndexStorage>>();
-    public boolean isEmpty(){
-
-        return false;
-    }
-
-    public void initMap(String word, String line, int pos){
-
-        IndexStorage index = new IndexStorage();
-        index.setLine(line);
-        index.setPosition(pos);
-
-        map.put(word, List.add(index));
-    }
-
-    public List<IndexStorage> ocurrencesOfWord(String word){
-        return map.get(word);
-    }
-
-    public List<String> sorteWords(){
-        List<String> list = new List<String>();
-        map.forEach((d) -> {
-            list.add(d);
-        });
-        Collections.sort(list);
-        return list;
-    }
+//    Map<String, List<IndexStorage>> map = new HashMap<String, List<IndexStorage>>();
+//    public boolean isEmpty(){
+//
+//        return false;
+//    }
+//
+//    public void initMap(String word, String line, int pos){
+//
+//        IndexStorage index = new IndexStorage();
+//        index.setLine(line);
+//        index.setPosition(pos);
+//
+//        map.put(word, List.add(index));
+//    }
+//
+//    public List<IndexStorage> ocurrencesOfWord(String word){
+//        return map.get(word);
+//    }
+//
+//    public List<String> sorteWords(){
+//        List<String> list = new List<String>();
+//        map.forEach((d) -> {
+//            list.add(d);
+//        });
+//        Collections.sort(list);
+//        return list;
+//    }
 }
